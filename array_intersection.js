360b39bcbfb6a43de368c93f69e906aac9e3c2b1
Seven Li-后会有七 <vinson7lhl@126.com>
2020-08-03 22:01:20 +0800

    提交：数组交集

--- array_intersection.js
+++ array_intersection.js
@@ -2,9 +2,9 @@ let result = []
 function array_intersection(arr1, arr2) {
   // 永远从短的数组进行遍历
   if (arr1.length < arr2.length) {
-    for(let i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr1.length; i++) {
       // 如果匹配上了
-      if(arr2.indexOf(arr1[i]) !== -1) {
+      if (arr2.indexOf(arr1[i]) !== -1) {
         let arr2_index = arr2.indexOf(arr1[i])
         result.push(arr1[i])
         // 剔除长数组重复元素得到新元素
@@ -15,8 +15,8 @@ function array_intersection(arr1, arr2) {
       i = -1
     }
   } else {
-    for(let i = 0; i < arr2.length; i++) {
-      if(arr1.indexOf(arr2[i]) !== -1) {
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) !== -1) {
         let arr1_index = arr1.indexOf(arr2[i])
         let arr2_index = i
         result.push(arr2[i])
